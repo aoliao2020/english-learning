b7d4073637df4317ca52d93c6e11e91888771045
mht37 <hangtongma@gmail.com>
2025-07-31 22:43:51 +0800

    fix: add vite react plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,10 @@
-import { defineConfig } from 'vite'; import react from '@vitejs/plugin-react'; export default defineConfig({ plugins: [react()] });
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // ⬅️ 确保有这个设置，默认也是 dist
+  }
+})
